9923305f4101780a3e5e4c58497d9edbddb88281
jai1726 <jagadeeshdende@gmail.com>
2024-10-29 01:55:40 +0530

    React App Setup with Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import react from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+        Girman Technologies
+    </div>
+  );
+}
+
+export default App;
